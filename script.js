34de524afa3c1947d121c4ed0d30705318899247
Milind Mishra⚡️ <milind.mishra4@gmail.com>
2022-05-07 12:03:07 +0530

    📦 NEW: logic added

---
+++ script.js
@@ -0,0 +1,12 @@
+let calc = () => {
+  // Get the data from element on the form.
+  const billAmt = document.getElementById("bill-amount");
+  const noOfPeople = document.getElementById("people");
+  console.log(`The bill amount is : ${billAmt.value}`);
+  console.log(`The number of peoples are : ${noOfPeople.value}`);
+  // Split the bill amount among the people.
+  const amountPerPerson = billAmt.value / noOfPeople.value;
+  // Display the amount per person in the HTML.
+  document.getElementById("all-in-total").innerHTML += billAmt.value;
+  document.getElementById("total-for-each").innerHTML += amountPerPerson;
+};
